adabbc6a9eb6f446acbe5c87cf13c51c20193da2
Chun-Sheng, Li <peter279k@gmail.com>
2020-03-02 02:29:20 +0800

    Test enhancement (#22)

--- tests/EnglishConversionTest.php
+++ tests/EnglishConversionTest.php
@@ -17,82 +17,86 @@ class EnglishConversionTest extends TestCase
         $this->converter = new Converter("naira", "kobo", Language::ENGLISH);
     }
 
-    public function testWholeNumber()
+    public function wholeNumberDataProvider()
+    {
+        return [
+            ["345", "three hundred and forty-five naira only"],
+            ["34", "thirty-four naira only"],
+            ["23455", "twenty-three thousand, four hundred and fifty-five naira only"],
+            ["345003", "three hundred and forty-five thousand, three naira only"],
+            ["475923455", "four hundred and seventy-five million, nine hundred and twenty-three thousand, four hundred and fifty-five naira only"],
+        ];
+    }
+
+    /**
+     * @dataProvider wholeNumberDataProvider
+     */
+    public function testWholeNumber($wholeNumber, $expectedMessage)
     {
         $this->assertEquals(
-            $this->converter->convert("345"),
-            "three hundred and forty-five naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("34"),
-            "thirty-four naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("23455"),
-            "twenty-three thousand, four hundred and fifty-five naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("345003"),
-            "three hundred and forty-five thousand, three naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("475923455"),
-            "four hundred and seventy-five million, nine hundred and twenty-three thousand, four hundred and fifty-five naira only"
+            $expectedMessage,
+            $this->converter->convert($wholeNumber)
         );
     }
 
-    public function testLargeNumbers()
+    public function largeNumbersDataProvider()
+    {
+        return [
+            ["50000000", "fifty million naira only"],
+            ["900000000000", "nine hundred billion naira only"],
+            ["900070000000", "nine hundred billion, seventy million naira only"],
+        ];
+    }
+
+    /**
+     * @dataProvider largeNumbersDataProvider
+     */
+    public function testLargeNumbers($largeNumber, $expectedMessage)
     {
         $this->assertEquals(
-            $this->converter->convert("50000000"),
-            "fifty million naira only"
-        );
-
-        $this->assertEquals(
-            $this->converter->convert("900000000000"),
-            "nine hundred billion naira only"
-        );
-
-        $this->assertEquals(
-            $this->converter->convert("900070000000"),
-            "nine hundred billion, seventy million naira only"
+            $expectedMessage,
+            $this->converter->convert($largeNumber)
         );
     }
 
-    public function testNumberWithZeroPrefix()
+    public function numberWithZeroPrefixDataProvider()
     {
-        $this->assertEquals($this->converter->convert("0000000"), "");
+        return [
+            ["0000000", ""],
+            ["050003", "fifty thousand, three naira only"],
+            ["050303", "fifty thousand, three hundred and three naira only"],
+            ["005475923455", "five billion, four hundred and seventy-five million, nine hundred and twenty-three thousand, four hundred and fifty-five naira only"],
+        ];
+    }
+
+    /**
+     * @dataProvider numberWithZeroPrefixDataProvider
+     */
+    public function testNumberWithZeroPrefix($numberWithZeroPrefix, $expectedMessage)
+    {
         $this->assertEquals(
-            $this->converter->convert("050003"),
-            "fifty thousand, three naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("050303"),
-            "fifty thousand, three hundred and three naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("005475923455"),
-            "five billion, four hundred and seventy-five million, nine hundred and twenty-three thousand, four hundred and fifty-five naira only"
+            $expectedMessage,
+            $this->converter->convert($numberWithZeroPrefix)
         );
     }
 
-    public function testDecimalNumber()
+    public function decimalNumberDataProvider()
+    {
+        return [
+            ["23.0", "twenty-three naira only"],
+            ["345003.09", "three hundred and forty-five thousand, three naira, nine kobo only"],
+            ["233464773.457", "two hundred and thirty-three million, four hundred and sixty-four thousand, seven hundred and seventy-three naira, forty-six kobo only"],
+        ];
+    }
+
+    /**
+     * @dataProvider decimalNumberDataProvider
+     */
+    public function testDecimalNumber($decimalNumber, $expectedMessage)
     {
         $this->assertEquals(
-            $this->converter->convert("23.0"),
-            "twenty-three naira only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("345003.09"),
-            "three hundred and forty-five thousand, three naira, nine kobo only"
-        );
-        $this->assertEquals(
-            $this->converter->convert("233464773.457"),
-            "two hundred and thirty-three million, four hundred and sixty-four thousand, seven hundred and seventy-three naira, forty-six kobo only"
+            $expectedMessage,
+            $this->converter->convert($decimalNumber)
         );
     }
 }
-
-// $money = "八百七十二万七千八百二十四";
-
-// echo ($this->converter->convert($money) . "\n");
